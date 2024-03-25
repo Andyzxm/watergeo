26e331378b6c4f74b6eb1bd21170691947f71781
Xinming Andy Zhang <andyzxm1101@gmail.com>
2024-03-25 13:05:06 -0400

    change map function

--- watergeo/watergeo.py
+++ watergeo/watergeo.py
@@ -1,11 +1,55 @@
 """Main module."""
 
 import ipyleaflet
+from ipyleaflet import basemaps
+
 
 class Map(ipyleaflet.Map):
 
-    def __init__(self, center=[20,0], zoom=2,**kwargs):
-        super().__init__(center=center, zoom=zoom,**kwargs)
-        self.add_control(ipyleaflet.LayersControl())
+    def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
+
+    def add_tile_layer(self, url, name, **kwargs):
+        layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
+        self.add(layer)
+
+    def add_basemap(self, name):
+
+        if isinstance(name, str):
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_tile_layer(url, name)
+        else:
+            self.add(name)
+
+    def add_layers_control(self, position="topright"):
+        self.add_control(ipyleaflet.LayersControl(position=position))
+
+    def add_geojson(self, data, name="geojson", **kwargs):
+
+        import json
+
+        if isinstance(data, str):
+            with open(data) as f:
+                data = json.load(f)
+
+        if "style" not in kwargs:
+            kwargs["style"] = {"color": "blue", "weight": 1, "fillOpacity": 0}
+
+        if "hover_style" not in kwargs:
+            kwargs["hover_style"] = {"fillColor": "#ff0000", "fillOpacity": 0.5}
+
+        layer = ipyleaflet.GeoJSON(data=data, name=name, **kwargs)
+        self.add(layer)
+
+    def add_shp(self, data, name="shp", **kwargs):
+
+        import shapefile
+        import json
+
+        if isinstance(data, str):
+            with shapefile.Reader(data) as shp:
+                data = shp.__geo_interface__
+
+        self.add_geojson(data, name, **kwargs)
 
     
