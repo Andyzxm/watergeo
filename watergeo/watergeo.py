70aa48303a8888354ae22370671d40d822fe4968
Xinming Andy Zhang <andyzxm1101@gmail.com>
2024-03-25 12:33:00 -0400

    update map function

--- watergeo/watergeo.py
+++ watergeo/watergeo.py
@@ -1,12 +1,12 @@
 """Main module."""
 
 import ipyleaflet
+from ipyleaflet import basemaps
 
 class Map(ipyleaflet.Map):
 
-    def __init__(self, center=[20,0], zoom=2,**kwargs):
-        super().__init__(center=center, zoom=zoom,**kwargs)
-        self.add_control(ipyleaflet.LayersControl())
+    def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
 
     def add_tile_layer(self, url, name, **kwargs):
         layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
