a288dc43de4b5c63cc2aac2a26079e8c9c66e6a9
Xinming Andy Zhang <andyzxm1101@gmail.com>
2024-04-22 20:36:16 -0400

    add_ee_to_folium

--- watergeo/foliumap.py
+++ watergeo/foliumap.py
@@ -1,5 +1,5 @@
 import folium
-
+import ee
 
 class Map(folium.Map):
 
@@ -22,3 +22,40 @@ class Map(folium.Map):
         client = TileClient(data)
         layer = get_folium_tile_layer(client, name=name, **kwargs)
         layer.add_to(self)
+    
+    def add_ee_layer(ee_object, vis_params={}, name="Layer untitled", shown=True, opacity=1.0):
+        """
+        Adds Earth Engine data layers to the map.
+    
+        Args:
+            ee_object (object): The Earth Engine object to add to the map.
+            vis_params (dict, optional): Visualization parameters. Defaults to {}.
+            name (str, optional): The name of the layer. Defaults to "Layer untitled".
+            shown (bool, optional): Whether to show the layer initially. Defaults to True.
+            opacity (float, optional): The opacity of the layer (between 0 and 1). Defaults to 1.0.
+        """
+        try:
+            ee.Initialize()  # Initialize Earth Engine
+            ee_object.getInfo()  # Check if the object is valid
+        except Exception as e:
+            print("Error adding Earth Engine layer:", e)
+            return
+    
+        if isinstance(ee_object, ee.ImageCollection):
+            ee_object = ee_object.mosaic()
+    
+        # Generate a URL for fetching the tiles from Earth Engine
+        map_id_dict = ee.Image(ee_object).getMapId(vis_params)
+    
+        # Create a new tile layer
+        tiles_url = map_id_dict['tile_fetcher'].url_format
+        map = folium.Map(location=[0, 0], zoom_start=3)
+        folium.TileLayer(
+            tiles=tiles_url,
+            attr='Google Earth Engine',
+            name=name,
+            overlay=True,
+            control=True
+        ).add_to(map)
+    
+        return map
